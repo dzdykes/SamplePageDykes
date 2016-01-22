e9bd90dd084367cd354af1f71deb268e4dafc5fe
Dylan Dykes <dzdykes88@gmail.com>
2016-01-22 16:09:34 -0500

    Added JavaScript to main menu. Added lightbox feature to media page.

---
+++ script/scripts.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+    $("li").hide();
+    $(".navHeader").mouseenter(function () {
+        $(this).css("color", "yellow")
+        $("li").slideDown();
+    });
+    $("nav").mouseleave(function () {
+        $(".navHeader").css("color", "white")
+        $("li").slideUp();
+    });
+});
